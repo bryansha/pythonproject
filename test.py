0489324138619059b8a730ed2b3d2e79dc3c911c
Bryan Ha <30011082+bryansha@users.noreply.github.com>
2022-01-02 14:36:29 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+import json
+import time
+   
+with open("list2.json", encoding="utf8") as jsonFile:
+    data = json.load(jsonFile)
+    jsonFile.close()
+
+
+def search():
+    print("Enter the keyword: ")
+    keyword = input()
+    starttime = time.time()
+    countsearch = 0
+    for i in data:
+        if (keyword) in i["title"].lower():
+            countsearch = countsearch +1
+            print(i['title'])
+    endtime = time.time()    
+    print(keyword + " appeared " + str(countsearch) + " times")        
+    print("Took " + str(endtime-starttime) + " seconds")
+
+go = True
+while (go):
+    print("Enter your command: ")
+    command = input()
+    if (command == "search"):
+        search()
+    else:
+        go = False
